17c2fba334c9d6e3d647a111158afd9d139f8e11
cep104 <candicelizabethpeters@gmail.com>
2020-11-08 23:19:17 -0600

    notes added

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-  new_array = []
+  new_array = [] #create a blank array
   array.each do |number|
-    new_array.push(number ** 2)
+    new_array.push(number ** 2) #sqaure the number and push it into the new array
   end
-  new_array
+  new_array #return the new array
 end
